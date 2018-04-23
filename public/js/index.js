405f39f64f4814a2e6510a2b51486b2f815c1086
German Flores <gfloresa@ucsd.edu>
2018-04-22 23:57:20 -0700

    bs

--- public/js/index.js
+++ public/js/index.js
@@ -12,23 +12,4 @@ function initializePage() {
 	// name listener
 	$("").click();
 
-	let $articles = $('#articles');
-
-	$("button").click(function(){
-	        $.getJSON("https://newsapi.org/v2/top-headlines?country=us&apiKey=4ee54ee8064c4fc4af6a51b03fe81d17", function(result){
-	            $.each(result, function(i, field){
-	                $($articles).append(field.title + " ");
-	            });
-	        });
-	    });
-	$.ajax({
-		dataType: 'json',
-		url: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=4ee54ee8064c4fc4af6a51b03fe81d17',
-		success: (articles) => {
-				$.each(articles, (i, article) => {
-					$articles.append(`<li>name: ${article.title}</li>`);
-				});
-		}
-	});
-
 }
