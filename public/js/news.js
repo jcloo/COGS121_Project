15cfbbeaae1ee56e336fab16d37dc6a051def678
Jan Eric De Castro <jedecast.edu>
2018-04-22 23:16:55 -0700

    minor changes to index and added news

--- public/js/news.js
+++ public/js/news.js
@@ -1,8 +1,32 @@
-var url = 'https://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          'apiKey=API_KEY';
-var req = new Request(url);
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
+
+$(document).ready(function() {
+	initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	console.log("Javascript connected!");
+	
+	$('.clickMe').click(gotData);
+}
+
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=bebd5673446e40c0a0613cd3ea327a8d";
+
+
+function setup() {
+	noCanvas();
+	loadJSON(url, gotData);
+}
+
+function gotData(data) {
+	var articles = data.response.docs;
+
+	for (var i = 0; i < articles.length; i++) {
+		createElement('h1', articles[i].headline.main);
+		createP(articles[i].snippet);
+	}
+}
+
